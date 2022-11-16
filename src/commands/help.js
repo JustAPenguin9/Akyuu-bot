97a68796bcb11dc497d141711f01cc50b298ccfc
Stephen <stephentc2005@gmail.com>
2022-11-15 20:09:57 -0500

    fix profile sending the message before the db returns the stats

--- src/commands/help.js
+++ src/commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
       .setTitle('Akyuu bot commands\nprefix: **!**')
       .addFields(
         { name: 'to get the data for a move type **![character] [move]**', value: '[move] is the move you want in numpad notation. you can use !wiki controls for more information on that.\nex: !reimu j5a' },
-        { name: "if you're new to AOCF, use **!starthere**", value: 'this will get you the links to SogofGod1998 starterpack complete with everything you need to play.' },
+        { name: "if you're new to AOCF, use **!starthere**", value: 'this will get you the links to SonofGod1998 starterpack complete with everything you need to play.' },
         { name: 'wiki', value: 'you can also use !wiki [option] to get to a spacific wiki page or just !wiki by itselft to go to the homepage.\nwiki options: any of the characters, controls, mechanics', inline: true },
         { name: 'wiki arguments', value: '[character]\ncontrols\nmechanics', inline: true },
         { name: 'multiplayer', value: 'win --you won against the other player\nloss --you lost against the other player\nprofile [user] --view your, or someone else\'s, profile\ndiscordID --save a file of your discord client id' },
--- src/commands/profile.js
+++ src/commands/profile.js
@@ -12,27 +12,27 @@ module.exports = {
       losses = (await bot.historyDb.query(`SELECT * FROM history WHERE loser = ${player.id}`)).length
     } catch (error) {
       message.channel.send('error recieving past results from the database: ' + error)
+    } finally {
+      let ratio = (wins / losses).toPrecision(2)
+      if (isNaN(ratio)) ratio = '0.0' // zero wins
+      if (ratio === 'Infinity') ratio = '1.0' // zero losses
+
+      const embed = new EmbedBuilder()
+        .setColor('#B3E04D')
+        .setTitle(`${player.username} win loss ratio`)
+        .setThumbnail(
+          player.avatarURL()
+            ? `${player.avatarURL()}?size=1024`
+            : player.displayAvatarURL({ dynamic: true })
+        )
+        .addFields(
+          { name: 'Wins', value: wins.toString(), inline: true },
+          { name: 'Losses', value: losses.toString(), inline: true },
+          { name: 'W/L Ratio', value: ratio.toString(), inline: true },
+          { name: 'Total games played', value: (wins + losses).toString(), inline: true }
+        )
+
+      message.reply({ embeds: [embed] })
     }
-
-    let ratio = (wins / losses).toPrecision(2)
-    if (isNaN(ratio)) ratio = '0.0' // zero wins
-    if (ratio === 'Infinity') ratio = '1.0' // zero losses
-
-    const embed = new EmbedBuilder()
-      .setColor('#B3E04D')
-      .setTitle(`${player.username} win loss ratio`)
-      .setThumbnail(
-        player.avatarURL()
-          ? `${player.avatarURL()}?size=1024`
-          : player.displayAvatarURL({ dynamic: true })
-      )
-      .addFields(
-        { name: 'Wins', value: wins.toString(), inline: true },
-        { name: 'Losses', value: losses.toString(), inline: true },
-        { name: 'W/L Ratio', value: ratio.toString(), inline: true },
-        { name: 'Total games played', value: (wins + losses).toString(), inline: true }
-      )
-
-    message.reply({ embeds: [embed] })
   }
 }

a4975515a9acf87ae1b9c6f9d5b9bee6d9f9ed16
penguin <justapenguin974@gmail.com>
2023-09-10 23:55:13 -0400

    Update starthere

--- src/commands/starthere.js
+++ src/commands/starthere.js
@@ -2,7 +2,9 @@ module.exports = {
   name: 'starthere',
   aliases: ['starterpack', 'start', 'sh'],
   run: (bot, message, args) => {
-    message.channel.send('Here\'s the link to **SonofGod\'s** starter pack: <https://drive.google.com/drive/folders/1SzdFtiB1sdAtnYtmvrsF0IBi6lXHNLl1?usp=sharing>' +
-    '\nHere\'s the link to the discord post explaining it all: <https://discord.com/channels/273513597622157322/273733523829948416/850940292185456701>')
+    message.channel.send(
+      "Here's the link to **SonofGod's** starter pack: <https://drive.google.com/drive/folders/1cVEb1kN7X8lOsXn6XTBN1aphOEsD6SA8?usp=sharing>" +
+      "\nHere's the link to the discord post explaining it all: <https://discord.com/channels/273513597622157322/273733523829948416/1140858504161267803>"
+    )
   }
 }

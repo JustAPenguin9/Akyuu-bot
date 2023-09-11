04ecf8e3dd2d7d5c27d1cf88ee4795e261bdbe90
penguin <justapenguin974@gmail.com>
2023-09-10 23:55:00 -0400

    Update links

--- src/commands/links.js
+++ src/commands/links.js
@@ -5,8 +5,10 @@ module.exports = {
     message.channel.send(
       '**Github repo:** <https://github.com/JustAPenguin9/Akyuu-bot>' +
       '\n**Google sheet:** <https://docs.google.com/spreadsheets/d/1SPHJUIq8Wi-OOJhNmgmCGrn9d7frfcjhJhWlpLT3ej0/edit?usp=sharing>' +
-      '\n**AOCF wiki:** <https://aocf.koumakan.jp/wiki/Antinomy_of_Common_Flowers_Wiki>' +
-      '\n**AOCF starter pack** <https://drive.google.com/drive/folders/1SzdFtiB1sdAtnYtmvrsF0IBi6lXHNLl1?usp=sharing>'
+      '\n**AoCF wiki:** <https://aocf.koumakan.jp/wiki/Antinomy_of_Common_Flowers_Wiki>' +
+      '\n**AoCF starter pack** <https://drive.google.com/drive/folders/1cVEb1kN7X8lOsXn6XTBN1aphOEsD6SA8?usp=sharing>' +
+      '\n**AoCF netplay patch** <http://lunarcast.net/aocf.php>' +
+      '\n**Proxypunch** <https://github.com/delthas/proxypunch/releases/latest>'
     )
   }
 }
